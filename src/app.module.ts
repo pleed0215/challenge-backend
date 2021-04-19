877c7c8e736cd0a5ade47a9228189ee2392156a9
Lee Eun Deok <pleed0215@gmail.com>
2021-04-19 23:39:44 +0900

    ?

---
+++ src/app.module.ts
@@ -0,0 +1,62 @@
+import { Module, RequestMethod, MiddlewareConsumer } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { PodcastsModule } from "./podcast/podcasts.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Podcast } from "./podcast/entities/podcast.entity";
+import { Episode } from "./podcast/entities/episode.entity";
+import { Review } from "./podcast/entities/review.entity";
+import { User } from "./users/entities/user.entity";
+import { UsersModule } from "./users/users.module";
+import { JwtModule } from "./jwt/jwt.module";
+import { JwtMiddleware } from "./jwt/jwt.middleware";
+import { AuthModule } from "./auth/auth.module";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      ...(process.env.NODE_ENV === "production"
+        ? {
+            type: "postgres",
+            ...(process.env.DATABASE_URL
+              ? {
+                  url: process.env.JAWSDB_URL,
+                  ssl: { rejectUnauthorized: false },
+                }
+              : {
+                  database: process.env.DB_NAME,
+                  host: process.env.DB_HOST,
+                  username: process.env.DB_USER,
+                  password: process.env.DB_PASSWORD,
+                  port: +process.env.DB_PORT,
+                  ssl: { rejectUnauthorized: false },
+                }),
+          }
+        : { type: "sqlite", database: "db.sqlite3" }),
+      synchronize: false,
+      logging: process.env.NODE_ENV !== "production",
+      entities: [Podcast, Episode, User, Review],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      context: ({ req }) => {
+        return { user: req["user"] };
+      },
+      playground: true,
+      introspection: true,
+    }),
+    JwtModule.forRoot({
+      privateKey: "8mMJe5dMGORyoRPLvngA8U4aLTF3WasX",
+    }),
+    PodcastsModule,
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware).forRoutes({
+      path: "/graphql",
+      method: RequestMethod.POST,
+    });
+  }
+}
